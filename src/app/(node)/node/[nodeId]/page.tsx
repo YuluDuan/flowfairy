c93c5a1180ac949102755dc166b9f891ae467ccb
UBC Student <2860128966@qq.com>
2023-09-03 21:56:59 -0700

    can save the pdf and the editor content

--- src/app/(node)/node/[nodeId]/page.tsx
+++ src/app/(node)/node/[nodeId]/page.tsx
@@ -3,18 +3,46 @@
 import Editor from "@/components/Editor";
 import LinkHeader from "@/components/LinkHeader";
 import UploadPdf from "@/components/UploadPdf";
+import { updateFlowInDatabase } from "@/lib/api-controllers";
+import useFlowStore from "@/store/useFlowStore";
+import { FlowFromDB, NodeDataType } from "@/types";
 import { LexicalEditor } from "lexical";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
-const LinkPage = () => {
+const LinkPage = ({ params }: { params: { nodeId: string } }) => {
   const editorRef = useRef<LexicalEditor>();
+  const [pdfFile, setPdfFile] = useState<any>(null);
 
-  const handleSavePDFandEditor = () => {};
+  const flow: FlowFromDB | null = useFlowStore((state) => state.flow);
+
+  const handleSavePDFandEditor = async () => {
+    //find the specific node data
+    if (flow) {
+      const nodes = flow!.flowData.nodes;
+      const nodeIndex = nodes.findIndex((node) => node.id === params.nodeId);
+      if (nodeIndex !== -1) {
+        const modifiedFlow: FlowFromDB = { ...flow };
+        const modifiedNodes = [...modifiedFlow.flowData!.nodes];
+        const nodeData: NodeDataType = modifiedNodes[nodeIndex].data;
+        nodeData.pdf = pdfFile;
+
+        if (editorRef.current) {
+          const editorState = editorRef.current;
+          const editorStateJSON = editorState.toJSON();
+          nodeData.editorContent = JSON.stringify(editorStateJSON);
+        }
+        // Update the modified nodes back into the modified flow
+        modifiedFlow.flowData!.nodes = modifiedNodes;
+        const updatedFlow = await updateFlowInDatabase(modifiedFlow);
+        console.log("save flow successfully", updatedFlow);
+      }
+    }
+  };
   return (
     <>
       <LinkHeader handleSavePDFandEditor={handleSavePDFandEditor} />
       <div className="flex w-full h-full">
-        <UploadPdf />
+        <UploadPdf pdfFile={pdfFile} setPdfFile={setPdfFile} />
         <Editor ref={editorRef} />
       </div>
     </>
