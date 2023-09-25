46d03a1195821f2206bf7cb507c89fc39336c336
UBC Student <2860128966@qq.com>
2023-09-24 23:32:10 -0700

    shift one level up

--- src/app/(main)/main/page.tsx
+++ src/app/(main)/main/page.tsx
@@ -1,5 +1,29 @@
+"use client";
+
 import SideNav from "@/components/SideNav";
+import { readFlowsFromDatabase } from "@/lib/api-controllers";
+import useFlowsStore from "@/store/useFlowsStore";
+import { useEffect } from "react";
 
 export default function Home() {
-  return <SideNav />;
+  const updateFlows = useFlowsStore((state) => state.updateFlows);
+  const flows = useFlowsStore((state) => state.flows);
+  useEffect(() => {
+    const getflows = async () => {
+      const data = await readFlowsFromDatabase();
+      updateFlows(data);
+    };
+
+    getflows().catch(console.error);
+  }, [updateFlows]);
+  return <SideNav flows={flows} />;
 }
+
+// import SideNav from "@/components/SideNav";
+// import { readFlowsFromDatabase } from "@/lib/api-controllers";
+// import useFlowsStore from "@/store/useFlowsStore";
+// import { useEffect } from "react";
+
+// export default function Home() {
+//   return <SideNav />;
+// }
